59aef274582a7fde21594973c62637e18a5398df
saydur8853 <saydur774@gmail.com>
2026-02-08 18:03:45 +0600

    smth get behind

--- obj/Debug/net8.0/VisorHR.Runner.AssemblyInfo.cs
+++ obj/Debug/net8.0/VisorHR.Runner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisorHR.Runner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf313c85cfb1f51246a0016677a20bb7d0018935")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisorHR.Runner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisorHR.Runner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/VisorHR.Api/obj/Debug/net8.0/VisorHR.Api.AssemblyInfo.cs
+++ src/VisorHR.Api/obj/Debug/net8.0/VisorHR.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisorHR.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf313c85cfb1f51246a0016677a20bb7d0018935")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisorHR.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisorHR.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
